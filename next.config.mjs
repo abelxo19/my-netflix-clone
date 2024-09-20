ae99b4bbb403b7c555884735ebc0a1023d7c42a5
abel <abelaatkelet@gmail.com>
2024-09-20 16:38:28 +0300

    final-commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+      },
+    ],
+  },
+};
 
+// Use the ES module export syntax
 export default nextConfig;
